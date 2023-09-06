7def412c3d55b64a3c42a03eb08dc15064b45516
Jonibek04 <142173538+Jonibek04@users.noreply.github.com>
2023-09-06 22:57:33 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 
-if a+b>10:
+if a+b > 10:
   print("Way too much")
 else:
   print("Balanced")
